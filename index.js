5dafdbaf88ec4bc27d4bbe103f1e4685c34e2e5c
Kao <trumblays@yahoo.com>
2021-12-08 02:45:11 -0600

    edited file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const path = require("path");
 const cool = require("cool-ascii-faces");
-const { Pool } = require("pg");
 const PORT = process.env.PORT || 5000;
+const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -10,7 +10,6 @@ const pool = new Pool({
     rejectUnauthorized: false,
   },
 });
-
 express()
   .use(express.static(path.join(__dirname, "public")))
   .set("views", path.join(__dirname, "views"))
@@ -22,12 +21,12 @@ express()
     try {
       const client = await pool.connect();
       const result = await client.query("SELECT * FROM test_table");
-      const results = { results: results ? result.rows : null };
+      const results = { results: result ? result.rows : null };
       res.render("pages/db", results);
       client.release();
     } catch (err) {
       console.error(err);
-      res.send("error" + err);
+      res.send("Error " + err);
     }
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
